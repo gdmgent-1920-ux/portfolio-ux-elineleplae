8fe8182ec0e06d8ad663f7f94366722aa4186fb7
elineleplae <elineleplae@users.noreply.github.com>
2020-02-28 22:00:56 +0100

    indienen opdracht 1: small application

--- data.js
+++ data.js
@@ -36,7 +36,7 @@ const data = {
             {
                 "text": "Adobe XD Prototype",
                 "title": "Mijn Adobe XD prototype (share link)",
-                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "href": "https://xd.adobe.com/view/5de1c07b-9926-43f2-7615-055fb2c59cee-2bd0/",
                 "target": "_blank"
             }
         ]
